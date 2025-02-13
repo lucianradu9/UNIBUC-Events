f6bc3ba1f25133ef0aef0edf0b7604048ac71d00
Lucian <lucian9radu@gmail.com>
2025-02-13 18:23:44 +0200

    v16
    Test baza de date

--- app/db_connection.php
+++ app/db_connection.php
@@ -1,9 +1,9 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "event_app";
-$port = 8889;
+$servername = "jfrpocyduwfg38kq.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
+$username = "hp1jxz1f3y09ptk6";
+$password = "htgsav0hj7rp6gnz";
+$dbname = "evavug5z2045gv8d";
+$port = 3306;
 
 // Creare conexiune
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
